705b8eed9a3f197a4177b5f2e20e70bb3e691c11
Hennadii_Shpak <borchuga@gmail.com>
2023-09-16 11:28:17 +0300

    initial commit

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  trailingComma: 'none',
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  bracketSpacing: true,
+  printWidth: 120,
+  arrowParens: 'avoid'
+};
---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'epm-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'epm social network';
+}
