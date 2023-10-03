7d5f69cb970637f9db6108ff92dc452a002583a6
Alexander Ponomarenko <101565736+ufgam@users.noreply.github.com>
2023-10-03 20:50:20 +0300

    added not-found-page

---
+++ src/app/shared/components/not-found/not-found.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { APP_ROUTER_NAME } from '../../../app.config';
+
+@Component({
+  selector: 'epm-not-found',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './not-found.component.html',
+  styleUrls: ['./not-found.component.scss']
+})
+export default class NotFoundComponent {
+  readonly linkTo: string[] = ['/', APP_ROUTER_NAME.main];
+}
