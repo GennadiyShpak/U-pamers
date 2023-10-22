c00fb43547fe2efa041dc94daaca75b79f5a84f8
Hennadii_Shpak <borchuga@gmail.com>
2023-10-22 14:16:38 +0300

    added preload straregy and hash url

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router';
+import { PreloadAllModules, RouterModule } from '@angular/router';
 import { importProvidersFrom } from '@angular/core';
 import { APP_ROUTES } from './app/app.routes';
 import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, {
-  providers: [importProvidersFrom(RouterModule.forRoot(APP_ROUTES))]
+  providers: [importProvidersFrom(RouterModule.forRoot(APP_ROUTES, {useHash: true, preloadingStrategy: PreloadAllModules}))]
 });
