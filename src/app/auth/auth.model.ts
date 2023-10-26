1f0fcf05821ede4bd7421cff3560cf47b302b041
Oleksandr Ponomarenko <79024231+Ponomaleks@users.noreply.github.com>
2023-10-26 15:37:59 +0300

    Add login form (#35)

--- src/app/auth/auth.model.ts
+++ src/app/auth/auth.model.ts
@@ -1,4 +1,5 @@
 import { STEPPER_STEPS } from './auth.config';
+import { FormControl } from '@angular/forms';
 
 export interface StepperConfig {
   activeStep: STEPPER_STEPS;
@@ -14,7 +15,13 @@ export interface ActionHandlerList {
   thirdStep: VoidCallback;
 }
 
+type NestedFormInterface<T, U> = {
+  [key in keyof T]: FormControl<U>;
+};
+
 export interface LoginData {
   email: string;
   password: string;
 }
+
+export type LoginForm = NestedFormInterface<LoginData, string>
--- src/app/auth/pages/log-in/log-in.component.ts
+++ src/app/auth/pages/log-in/log-in.component.ts
@@ -1,25 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { APP_ROUTER_NAME, BUTTON_THEMES } from '../../../app.config';
+import { APP_ROUTER_NAME, BUTTON_THEMES, INPUT_TYPES } from '../../../app.config';
 import { Router, RouterLink } from '@angular/router';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 import { EpmInputComponent } from '../../../shared/components/epm-input/epm-input.component';
 import { EpmButtonComponent } from '../../../shared/components/epm-button/epm-button.component';
+import { LoginForm } from '../../auth.model';
 
 @Component({
   selector: 'epm-log-in',
   standalone: true,
-  imports: [CommonModule, RouterLink, EpmInputComponent, EpmButtonComponent],
+  imports: [CommonModule, RouterLink, EpmInputComponent, EpmButtonComponent, ReactiveFormsModule],
   templateUrl: './log-in.component.html',
   styleUrls: ['./log-in.component.scss']
 })
-export class LogInComponent {
+export class LogInComponent implements OnInit {
+  loginForm!: FormGroup<LoginForm>;
+
   readonly appRoutes: typeof APP_ROUTER_NAME = APP_ROUTER_NAME;
   readonly buttonThemes: typeof BUTTON_THEMES = BUTTON_THEMES;
+  readonly inputTypes: typeof INPUT_TYPES = INPUT_TYPES;
 
-  constructor(private router: Router) {}
+  get email(): FormControl {
+    return this.loginForm.controls.email;
+  }
+  get password(): FormControl {
+    return this.loginForm.controls.password;
+  }
+
+  constructor(
+    private router: Router,
+    private fb: FormBuilder
+  ) {}
+
+  ngOnInit(): void {
+    this.initLoginForm();
+  }
 
   onLogIn(): void {
     this.router.navigateByUrl(`/${this.appRoutes.Main}`);
+  }
+
+  private initLoginForm(): void {
+    this.loginForm = this.fb.group<LoginForm>({
+      email: new FormControl('', {
+        nonNullable: true,
+        validators: [Validators.required]
+      }),
+      password: new FormControl('', {
+        nonNullable: true,
+        validators: [Validators.required]
+      })
+    });
   }
 }
