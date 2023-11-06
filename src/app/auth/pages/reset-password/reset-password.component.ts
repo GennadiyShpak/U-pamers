4399b725b49ef7fc066dcecead258bc1da269388
Oleksandr Ponomarenko <79024231+Ponomaleks@users.noreply.github.com>
2023-11-06 13:54:46 +0200

    add form, validation, error-msg, improve styles (#51)
    * add form, validation, error-msg, improve styles
    
    * fix based on comments

--- src/app/auth/pages/reset-password/reset-password.component.ts
+++ src/app/auth/pages/reset-password/reset-password.component.ts
@@ -1,23 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormControl, Validators } from '@angular/forms';
+
 import { EpmInputComponent } from '../../../shared/components/epm-input/epm-input.component';
 import { EpmButtonComponent } from '../../../shared/components/epm-button/epm-button.component';
+import { EpmErrorMessageComponent } from '../../../shared/components/epm-error-message/epm-error-message.component';
 import { BUTTON_THEMES } from '../../../app.config';
-import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'epm-reset-password',
   standalone: true,
-  imports: [CommonModule, EpmInputComponent, EpmButtonComponent, FormsModule],
+  imports: [CommonModule, EpmInputComponent, EpmButtonComponent, EpmErrorMessageComponent, ReactiveFormsModule],
   templateUrl: './reset-password.component.html',
   styleUrls: ['./reset-password.component.scss']
 })
-export default class ResetPasswordComponent {
+export default class ResetPasswordComponent implements OnInit {
   email!: string;
   isResetFormSubmitted = false;
+  emailControl!: FormControl<string>;
+
   readonly buttonThemes: typeof BUTTON_THEMES = BUTTON_THEMES;
 
+  ngOnInit(): void {
+    this.initFormControl();
+  }
+
+  private initFormControl(): void {
+    this.emailControl = new FormControl('', {
+      nonNullable: true,
+      validators: [Validators.required, Validators.email]
+    });
+  }
+
   onSubmit(): void {
+    this.email = this.emailControl.value;
     this.isResetFormSubmitted = !this.isResetFormSubmitted;
   }
 }
