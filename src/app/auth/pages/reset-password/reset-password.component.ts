763b33d46631ba12e36cc08730f4813573eec6a6
Oleksandr Ponomarenko <ponomaleks@gmail.com>
2023-11-06 12:21:40 +0200

    fix based on comments

--- src/app/auth/pages/reset-password/reset-password.component.ts
+++ src/app/auth/pages/reset-password/reset-password.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormControl, Validators } from '@angular/forms';
+
 import { EpmInputComponent } from '../../../shared/components/epm-input/epm-input.component';
 import { EpmButtonComponent } from '../../../shared/components/epm-button/epm-button.component';
-import { BUTTON_THEMES } from '../../../app.config';
-import { ReactiveFormsModule, FormControl, Validators } from '@angular/forms';
 import { EpmErrorMessageComponent } from '../../../shared/components/epm-error-message/epm-error-message.component';
+import { BUTTON_THEMES } from '../../../app.config';
 
 @Component({
   selector: 'epm-reset-password',
@@ -13,17 +14,25 @@ import { EpmErrorMessageComponent } from '../../../shared/components/epm-error-m
   templateUrl: './reset-password.component.html',
   styleUrls: ['./reset-password.component.scss']
 })
-export default class ResetPasswordComponent {
+export default class ResetPasswordComponent implements OnInit {
   email!: string;
   isResetFormSubmitted = false;
-  emailControl: FormControl<string> = new FormControl('', {
-    nonNullable: true,
-    validators: [Validators.required, Validators.email]
-  });
+  emailControl!: FormControl<string>;
+
   readonly buttonThemes: typeof BUTTON_THEMES = BUTTON_THEMES;
 
+  ngOnInit(): void {
+    this.initFormControl();
+  }
+
+  private initFormControl(): void {
+    this.emailControl = new FormControl('', {
+      nonNullable: true,
+      validators: [Validators.required, Validators.email]
+    });
+  }
+
   onSubmit(): void {
-    console.log(this.emailControl.value);
     this.email = this.emailControl.value;
     this.isResetFormSubmitted = !this.isResetFormSubmitted;
   }
