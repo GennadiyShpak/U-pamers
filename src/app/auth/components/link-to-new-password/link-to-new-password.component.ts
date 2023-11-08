922c64e9e1b55ec7c13084263f07a384f9644d2e
Hennadii Shpak <61782411+GennadiyShpak@users.noreply.github.com>
2023-11-08 11:48:35 +0200

    added logic by reset password (#55)

---
+++ src/app/auth/components/link-to-new-password/link-to-new-password.component.ts
@@ -0,0 +1,21 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { APP_ROUTER_NAME } from '../../../app.config';
+
+@Component({
+  selector: 'epm-link-to-new-password',
+  standalone: true,
+  imports: [CommonModule, RouterLink],
+  template: `<p class="content">
+    <span *ngIf="description">{{ description }}</span>
+    <a class="link" [routerLink]="['/', routerName.Auth, routerName.NewPassword]">{{ linkDescription }}</a>
+  </p>`,
+  styleUrls: ['./link-to-new-password.component.scss']
+})
+export class LinkToNewPasswordComponent {
+  @Input({ required: true }) linkDescription = '';
+  @Input() description = '';
+
+  public readonly routerName: typeof APP_ROUTER_NAME = APP_ROUTER_NAME;
+}
--- src/app/auth/pages/reset-password/reset-password.component.ts
+++ src/app/auth/pages/reset-password/reset-password.component.ts
@@ -1,16 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule, FormControl, Validators } from '@angular/forms';
+import { tap } from 'rxjs';
 
 import { EpmInputComponent } from '../../../shared/components/epm-input/epm-input.component';
 import { EpmButtonComponent } from '../../../shared/components/epm-button/epm-button.component';
 import { EpmErrorMessageComponent } from '../../../shared/components/epm-error-message/epm-error-message.component';
 import { BUTTON_THEMES } from '../../../app.config';
+import { CognitoService } from '../../../services/cognito.service';
+import { LinkToNewPasswordComponent } from '../../components/link-to-new-password/link-to-new-password.component';
 
 @Component({
   selector: 'epm-reset-password',
   standalone: true,
-  imports: [CommonModule, EpmInputComponent, EpmButtonComponent, EpmErrorMessageComponent, ReactiveFormsModule],
+  imports: [
+    CommonModule,
+    EpmInputComponent,
+    EpmButtonComponent,
+    EpmErrorMessageComponent,
+    ReactiveFormsModule,
+    LinkToNewPasswordComponent
+  ],
   templateUrl: './reset-password.component.html',
   styleUrls: ['./reset-password.component.scss']
 })
@@ -20,6 +30,8 @@ export default class ResetPasswordComponent implements OnInit {
   emailControl!: FormControl<string>;
 
   readonly buttonThemes: typeof BUTTON_THEMES = BUTTON_THEMES;
+
+  constructor(private cognitoService: CognitoService) {}
 
   ngOnInit(): void {
     this.initFormControl();
@@ -34,6 +46,9 @@ export default class ResetPasswordComponent implements OnInit {
 
   onSubmit(): void {
     this.email = this.emailControl.value;
-    this.isResetFormSubmitted = !this.isResetFormSubmitted;
+    this.cognitoService
+      .forgotPassword(this.email)
+      .pipe(tap(() => (this.isResetFormSubmitted = true)))
+      .subscribe();
   }
 }
